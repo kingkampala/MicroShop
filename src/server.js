043c53a7f9f457d2f5dca4da6dfd520b37ad5ab5
Kampala <ezeanyakampala@gmail.com>
2024-07-30 17:08:01 +0100

    committed

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ const { app, connectDb } = require('../src/app');
 require('dotenv').config();
 
 const { MONGO_URL } = process.env;
-const port = process.env.PORT || 2810;
+const port = process.env.PORT || 4000;
 
 const startServer = async () => {
     try {
       await connectDb(MONGO_URL);
   
       app.listen(port, () => {
-        console.log(`authentication service running on port ${port}`);
+        console.log(`server running on port ${port}`);
       });
     } catch (error) {
       console.error('error starting the server:', error);
