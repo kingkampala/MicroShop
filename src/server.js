884e51fe9b9833d113c15a749b30a0ab8db68f85
Kampala <ezeanyakampala@gmail.com>
2024-08-04 23:30:13 +0100

    just commit

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 const { app, connectDb } = require('../src/app');
 require('dotenv').config();
 
-const { MONGO_URL } = process.env;
-const port = process.env.PORT || 2810;
+/*const { MONGO_URL } = process.env;
+const port = process.env.PORT || 2810;*/
+
+const { MONGO_URL, PORT = 2810 } = process.env;
+
+console.log(`Starting server with MONGO_URL: ${MONGO_URL}`);
+console.log(`Listening on port: ${PORT}`);
 
 const startServer = async () => {
     try {
       await connectDb(MONGO_URL);
   
-      app.listen(port, '0.0.0.0', () => {
-        console.log(`server running on port ${port}`);
+      app.listen(PORT, '0.0.0.0', () => {
+        console.log(`server running on port ${PORT}`);
       });
     } catch (error) {
       console.error('error starting the server:', error);
